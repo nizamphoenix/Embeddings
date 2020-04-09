6c74d154c9c12601c266983209a4a3609aaa0056
Nizamuddin <nizamphoenix@gmail.com>
2020-04-09 20:53:24 +1000

    Update BERT_fine_tuning.py

--- BERT/BERT_fine_tuning.py
+++ BERT/BERT_fine_tuning.py
@@ -29,6 +29,7 @@ class BERTBaseUncased(nn.Module):
         self.bert=transformers.BertModel.from_pretrained(self.bert_path)
         self.bert_drop=nn.Dropout(0.3)
         self.out=nn.Linear(768,30)
+        
     def forward(self,ids,mask,token_type_ids):
         _,o2=self.bert(ids,attention_mask=mask,token_type_ids=token_type_ids)
         bo=self.bert_drop(o2)
